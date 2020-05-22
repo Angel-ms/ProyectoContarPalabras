9c7d4d9c327e21d8145a123438dd040fed9b0993
alumno <alumno@DESKTOP-4MLIONK.localdomain>
2020-05-22 21:52:20 +0200

    Added javadoc for FormateadoSalida

--- ProyectoContarPalabras/src/com/FormateadoSalida.java
+++ ProyectoContarPalabras/src/com/FormateadoSalida.java
@@ -1,12 +1,31 @@
 package com;
-
+/**
+ * 
+ * @author alumno
+ *
+ */
 public class FormateadoSalida {
+	/**
+	 * 
+	 * @param total número de caracteres
+	 * @return texto indicando el número de caracteres
+	 */
 	public static String textoCaracteresTotal (int total) {
 		return("Hay " + total + " letra" + ((total==1)? "" : "s" ));
 	}
+	/**
+	 * 
+	 * @param total número de palabras
+	 * @return texto indicando el número de palabras
+	 */
 	public static String textoNumeroPalabras (int total) {
 		return("Hay " + total + " palabra" + ((total==1)? "" : "s" ));
 	}
+	/**
+	 * 
+	 * @param caracteres lista de caracteres y sus respectivas cantidades
+	 * @return texto indicando la cantidad que hay de cada caracter
+	 */
 	public static String textoTodosCaracteres (int[] caracteres) {
 		String listaCaracteres= "";
 		for(int i=0; i<caracteres.length;i++) {
