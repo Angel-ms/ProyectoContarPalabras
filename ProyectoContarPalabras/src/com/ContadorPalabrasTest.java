65684ca0d96a0e9ef8126f85cfee98b2be1437bf
alumno <alumno@DESKTOP-4MLIONK.localdomain>
2020-05-22 22:01:06 +0200

    Added javadoc for ContadorPalabrasTest

--- ProyectoContarPalabras/src/com/ContadorPalabrasTest.java
+++ ProyectoContarPalabras/src/com/ContadorPalabrasTest.java
@@ -3,27 +3,47 @@ package com;
 import static org.junit.jupiter.api.Assertions.*;
 
 import org.junit.jupiter.api.Test;
-
+/**
+ * 
+ * @author alumno
+ *
+ */
 class ContadorPalabrasTest {
-
+	
 	@Test
+	/**
+	 * Comprueba que cuenta correctamente los caracteres si no se incluyen espacios
+	 */
 	void contarCaracteresTotalTestNormal() {
 		assertEquals (ContadorPalabras.contarCaracteresTotal("test"),4);
 	}
 	@Test
+	/**
+	 * Comprueba que cuenta correctamente los caracteres habiendo espacios
+	 */
 	void contarCaracteresTotalTestEspacios() {
 		assertEquals (ContadorPalabras.contarCaracteresTotal("not blank spaces"),14);
 	}
 	
 	@Test
+	/**
+	 * Comprueba que cuenta correctamente las palabras si solo hay una
+	 */
 	void contarNumeroPalabrasTestSimple() {
 		assertEquals (ContadorPalabras.contarNumeroPalabras("test"),1);
 	}
 	@Test
+	/**
+	 * Comprueba que cuenta correctamente las palabras si hay varias
+	 */
 	void contarNumeroPalabrasTestMultiple() {
 		assertEquals (ContadorPalabras.contarNumeroPalabras("aqui hay muchas palabras"),4);
 	}
+	
 	@Test
+	/**
+	 * Comprueba que crea la lista correctamente si no hay ningún caracter
+	 */
 	void contarTodosCaracteresTestVacio() {
 		int[] arrayResultado = ContadorPalabras.contarTodosCaracteres("");
 		boolean esVacio = true;
@@ -37,6 +57,9 @@ class ContadorPalabrasTest {
 	}
 
 	@Test
+	/**
+	 * Comprueba que crea la lista correctamente si hay algun caracter
+	 */
 	void contarTodosCaracteresTest1() {
 		int[] arrayResultado = ContadorPalabras.contarTodosCaracteres("1");
 		boolean esCorrecto = true;
