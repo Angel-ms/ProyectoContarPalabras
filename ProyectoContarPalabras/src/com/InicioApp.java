2bb35d0b0c8903bd182835602ee74557d535d919
alumno <alumno@DESKTOP-4MLIONK.localdomain>
2020-05-22 20:07:43 +0200

    Counting functionalities moved to another class

--- ProyectoContarPalabras/src/com/InicioApp.java
+++ ProyectoContarPalabras/src/com/InicioApp.java
@@ -6,53 +6,12 @@ public class InicioApp {
 		String frase;
 		System.out.println("Introduce la cadena");
 		frase = input.nextLine();
-		System.out.println(textoCaracteresTotal(contarCaracteresTotal(frase)));
-		System.out.println(textoNumeroPalabras(contarNumeroPalabras(frase)));
-		System.out.println(textoTodosCaracteres(contarTodosCaracteres(frase)));
+		System.out.println(textoCaracteresTotal(ContadorPalabras.contarCaracteresTotal(frase)));
+		System.out.println(textoNumeroPalabras(ContadorPalabras.contarNumeroPalabras(frase)));
+		System.out.println(textoTodosCaracteres(ContadorPalabras.contarTodosCaracteres(frase)));
 		input.close();
 		}
 	
-	public static int contarCaracteresTotal(String cadena) {
-		int numeroDeLetras=0;
-		for(int i= 0; i< cadena.length(); i++) {
-			if(cadena.charAt(i) != ' ') {
-				numeroDeLetras++;
-			}
-		}		
-		return numeroDeLetras;
-	}
-	public static int contarNumeroPalabras(String cadena) {
-		int numeroDePalabras=0;
-		char caracterAnterior;
-		cadena=cadena.trim();
-		
-		if(!(cadena.equals(""))) {
-			caracterAnterior=' ';
-			numeroDePalabras++;
-			for(int i= 0; i< cadena.length(); i++) {
-				if (cadena.charAt(i) == ' ' && caracterAnterior!=' ') {
-					numeroDePalabras++;
-				}
-				caracterAnterior=cadena.charAt(i);
-			}
-		}
-		return numeroDePalabras;
-		
-	}
-	public static int[] contarTodosCaracteres(String cadena) {
-		int[] unicode = new int[65536];
-		String cadenaSinTrim = cadena;
-		cadena=cadena.trim();
-		for(int i=0; i<unicode.length;i++) {
-			unicode[i]=0;
-		}
-		unicode[(int)(' ')]=(cadenaSinTrim.length()-(cadena.length()));
-		
-		for(int i=0; i< cadena.length(); i++) {
-			unicode[(int)(cadena.charAt(i))]++;		
-		}
-		return unicode;
-	}
 	
 	public static String textoCaracteresTotal (int total) {
 		return("Hay " + total + " letra" + ((total==1)? "" : "s" ));
