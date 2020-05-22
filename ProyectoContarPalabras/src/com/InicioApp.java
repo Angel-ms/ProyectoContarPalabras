8eb80793ab33f83cd2fbc154cc475424c731fc59
alumno <alumno@DESKTOP-4MLIONK.localdomain>
2020-05-22 21:39:43 +0200

    Added javadoc for InicioApp

--- ProyectoContarPalabras/src/com/InicioApp.java
+++ ProyectoContarPalabras/src/com/InicioApp.java
@@ -1,6 +1,17 @@
 package com;
+
 import java.util.Scanner;
+
+/**
+ * 
+ * @author alumno
+ *
+ */
 public class InicioApp {
+/**
+ * Clase que recibe por teclado una cadena de caracteres y organiza al resto de clases para contar caracteres y palabras 
+ * @param args
+ */
 	public static void main(String[] args) {
 		Scanner input = new Scanner(System.in);
 		String frase;
